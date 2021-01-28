3431c6ac0a5d9795a3a3edf4ac083786be9eecb3
Daniel Lee <47399650+iproudyou@users.noreply.github.com>
2021-01-28 05:49:41 -0500

    add graphql api (#2)

--- src/modules/articles/pages/News.js
+++ src/modules/articles/pages/News.js
@@ -3,14 +3,19 @@ import { useQuery } from 'react-query';
 import Header from '../../common/pages/Header';
 import newsArticleStyle from '../styles/newsArticle';
 import NewsArticle from '../components/newsArticle';
-import { getAllNewsApi } from '../stores/newsApi';
+// import { getAllNewsApi } from '../stores/newsApi';
+import { allArticles } from '../stores/graphql';
 import CircularIndeterminate from '../../common/components/progressBar';
 
 export default function News() {
   const classes = newsArticleStyle();
   const { data, isLoading } = useQuery('news', async () => {
-    const { data } = await getAllNewsApi();
-    return data.data;
+    // RESTful API
+    // const { data } = await getAllNewsApi();
+    
+    // GraphQL API
+    const { data } = await allArticles();
+    return data.data.allArticles;
   });
   
   return (
--- src/modules/articles/stores/newsApi.js
+++ src/modules/articles/stores/newsApi.js
@@ -4,7 +4,7 @@ export const pullNewsApi = async () => {
     try {
         return await customAxios({
             method: 'post',
-            url: '/api/news',
+            url: '/api/articles',
         })
     } catch (err) {
         throw new Error();
@@ -15,7 +15,7 @@ export const getAllNewsApi = async () => {
     try {
         return await customAxios({
             method: 'get',
-            url: '/api/news',
+            url: '/api/articles',
         })
     } catch (err) {
         throw new Error();
